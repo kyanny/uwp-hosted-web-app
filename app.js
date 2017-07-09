f39491290d9249108ac26ea1051ea64b073a3a28
Kensuke Nagae <kyanny@gmail.com>
2017-07-10 02:26:58 +0900

    Test

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ if (typeof Windows !== "undefined") {
     });
     button.removeAttribute('disabled');
     console.log('hello');
+
+
+    var notifications = Windows.UI.Notifications;
+    var template = notifications.ToastTemplateType.toastImageAndText01;
+    var toastXml = notifications.ToastNotificationManager.getTemplateContent(template);
+    var toastTextElements = toastXml.getElementsByTagName("text");
+    toastTextElements[0].appendChild(toastXml.createTextNode("Toast from App"));
+    var toastImageElements = toastXml.getElementsByTagName("image");
+    toastImageElements[0].setAttribute("src", "https://assets.codepen.io/assets/social/facebook-default.png");
+    toastImageElements[0].setAttribute("alt", "red graphic");
+    var toast = new notifications.ToastNotification(toastXml);
+    var toastNotifier = notifications.ToastNotificationManager.createToastNotifier();
+    toastNotifier.show(toast);
+
 } else {
     console.log('sorry');
 }
